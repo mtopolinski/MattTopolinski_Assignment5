7c058927773d9d4d5df793007bcbe95756617a8e
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-03-15 18:29:23 -0500

    Add files via upload

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,1 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var ctrlItems = require('../controllers/items');
 
+/* Items list page */
+router.get('/', ctrlItems.itemList);
+
+module.exports = router;
